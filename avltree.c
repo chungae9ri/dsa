ec6c967bf3c46fb1993b646baa250c08aa439aad
Kwangdo Yi <chungae9ri@gmail.com>
2015-08-06 15:15:12 -0500

    AVL tree insert/delete/search routine implemented.

---
+++ avltree.c
@@ -0,0 +1,568 @@
+/* 
+   pretty heuristic in deletion. need to consider corner cases especailly in deletion.  
+   insert is for recursive insertion. need to update the left/right pointer, 
+   so address of left/right pointer should be delivered.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX(A, B) (((A)>(B))?(A):(B))
+
+struct tnode{
+	int key; /* key */
+	char name[16];
+	/* doubly linked list. par is to parent. */
+	struct tnode *par, *left, *right;
+	int ht;  /* height, bal = r->ht - l->ht */
+	int inc;
+};
+
+struct tnode *root = NULL;
+
+void setht(struct tnode *pn)
+{
+
+	if(!pn->left && pn->right) pn->ht = pn->right->ht + 1;
+	else if(!pn->right && pn->left) pn->ht = pn->left->ht + 1;
+	else if(!pn->right && !pn->left) pn->ht = 0;
+	else pn->ht = MAX(pn->right->ht, pn->left->ht) + 1;
+}
+
+/* rotate right for zig-zig (A, B, C) */
+/*       a             b
+        /             / \
+       b     =>      c   a
+      /
+     c
+ */
+void rr(struct tnode *pn)
+{
+	struct tnode *cur = pn;
+	struct tnode *temp1 = cur->left; /* for right child */
+
+	if(cur->par) {
+		if(cur->par->right == cur) {
+			cur->par->right = temp1;
+			temp1->par= cur->par;
+		} else {
+			cur->par->left = temp1;
+			temp1->par = cur->par;
+		}
+
+	} else {
+		root = temp1;
+		temp1->par = NULL;
+	}
+
+	if(temp1->right) {
+		cur->left = temp1->right;
+		temp1->right->par = cur;
+	} else cur->left = NULL;
+
+	temp1->right = cur;
+	cur->par = temp1;
+
+	setht(cur);
+	setht(temp1);
+}
+
+/* rotate left for zag-zag (A, B, C) */
+/* a                 b
+    \               / \ 
+     b     =>      a   c
+      \
+       c
+ */
+void rl(struct tnode *pn)
+{
+	struct tnode *cur = pn;
+	struct tnode *temp1 = cur->right; /* for right child */
+
+	if(cur->par) {
+		if(cur->par->right == cur) {
+			cur->par->right = temp1;
+			temp1->par= cur->par;
+		} else {
+			cur->par->left = temp1;
+			temp1->par = cur->par;
+		}
+
+	} else {
+		root = temp1;
+		temp1->par = NULL;
+	}
+
+	if(temp1->left) {
+		cur->right = temp1->left;
+		temp1->left->par = cur;
+	} else cur->right = NULL;
+
+	temp1->left = cur;
+	cur->par = temp1;
+
+	setht(cur);
+	setht(temp1);
+}
+
+/* double rotate right and left for zig-zag(A, B, C) */
+/*
+	a              a                 a
+       / \            / \               / \
+      b   e   =>     b   e     =>      d   e
+       \              \               / \
+        c              d             b   c
+       /                \
+      d                  c
+ */
+void rrl(struct tnode *pn)
+{
+	struct tnode *cur = pn;
+	struct tnode *temp1 = cur->right;
+	struct tnode *temp2 = temp1->left;
+
+	rr(temp1);
+	rl(cur);
+	setht(cur);
+	setht(temp2);
+}
+
+/* double rotate left and right for zig-zag(A, B, C) */
+/*
+       a                a                  a
+      / \              / \                / \
+     e   b    =>      e   b       =>     e   d
+	/                /                  / \
+       c                d                  c   b
+        \              / 
+	 d            c
+ */
+void rlr(struct tnode *pn)
+{
+	struct tnode *cur = pn;
+	struct tnode *temp1 = cur->right;
+	struct tnode *temp2 = temp1->left;
+
+	rl(temp1);
+	rr(cur);
+	setht(cur);
+	setht(temp2);
+
+}
+
+void xinsert(struct tnode **pn, struct tnode tn)
+{
+	struct tnode *cur = *pn, *temp/* for swap */, *temp2, *temp3;
+	int bal = 0;
+	if(!cur) {
+		cur = (struct tnode*)malloc(sizeof(struct tnode));
+		cur->left = NULL;
+		cur->right = NULL;
+		cur->key = tn.key;
+		strcpy(cur->name, tn.name);
+		/*cur->bal = 0;*/
+		cur->ht = 0;
+		cur->inc = 0;
+		*pn = cur;
+		return;
+	} else {
+		/* height increase when both children are null */
+		if(!cur->left && !cur->right) {
+			cur->ht += 1;
+			cur->inc = 1;
+		} else cur->inc = 0;
+
+		if(tn.key < cur->key) {
+			xinsert(&cur->left, tn);
+			/* need to update height after insert. this is reverse order */
+			if(!cur->left->par) cur->left->par = cur;
+			if(cur->left->inc) {
+				cur->left->inc = 0;
+				setht(cur);
+				cur->inc = 1;
+			}
+		} else {
+			xinsert(&cur->right, tn);
+			/* need to update height after insert. this is reverse order */
+			if(!cur->right->par) cur->right->par = cur;
+			if(cur->right->inc) {
+				cur->right->inc = 0;
+				setht(cur);
+				cur->inc = 1;
+			}
+		}
+
+		if(!cur->left && !cur->right) bal = 0;
+		else if(!cur->left) bal = cur->right->ht - (-1); /* height of NULL is -1 */
+		else if(!cur->right) bal = (-1) - cur->left->ht; /* height of NULL is -1 */
+		else bal = cur->right->ht - cur->left->ht;
+		/* need to check balance factor after recursive */
+		/* if balancing is broken */
+		if(bal == 2) {
+			if(tn.key >= cur->right->key) {
+				/* case 1 : right-right */
+				rl(cur);
+				cur->inc = 0;
+			} else { 
+				/* case 2 : right-left */
+				rrl(cur);
+				cur->inc = 0;
+			} 
+		} else if(bal == -2) {
+			if(tn.key < cur->left->key) {
+				/* case 3: left - left */
+				rr(cur);
+				cur->inc = 0;
+			} else {
+				/* case 4: left-right */
+				rlr(cur);
+				cur->inc = 0;
+			} 
+		}
+	}
+}
+
+struct tnode *findpredecessor(struct tnode *pn)
+{
+	struct tnode *cur;
+
+	if(!pn) return NULL;
+	
+	cur = pn->left;
+	if(!cur) return NULL;
+
+	/* specially in this example, the node with biggest key of left children is the predecessor */
+	while(cur->right) {
+		cur = cur->right;
+	}
+
+
+	return cur;
+}
+
+struct tnode *findsuccessor(struct tnode *pn)
+{
+	struct tnode *cur;
+	if(!pn) return NULL;
+	
+	cur = pn->right;
+	if(!cur) return NULL;
+	while(cur->left) {
+		cur = cur->left;
+	}
+	return cur;
+}
+
+void xdelete(struct tnode **pn, struct tnode tn)
+{
+	int bal = 0;
+	struct tnode *cur = *pn;
+	struct tnode *successor;
+	struct tnode *temp;
+
+	if(!cur) {
+		return;
+	} else {
+		if(cur->key == tn.key) {
+			/* case 1 : if cur is leaf*/
+			if(!cur->left && !cur->right) {
+				if(!cur->par) {/* there is only a root node */
+					free(cur);
+					cur = NULL;
+					root = NULL;
+				} else if(cur->par->left == cur) {
+					cur->par->left = NULL;
+					free(cur);
+					cur = NULL;
+				} else if(cur->par->right == cur) {
+					cur->par->right = NULL;
+					free(cur);
+					cur = NULL;
+				} 
+				/* case 2 : if cur has only one child */
+			} else if(cur->right && !cur->left) {
+				if(cur->par) {
+					if(cur->par->left == cur) {
+						cur->par->left = cur->right;
+						cur->right->par = cur->par;
+					}
+					else {
+						cur->par->right = cur->right;
+						cur->right->par = cur->par;
+					}
+				} else {/* corner case when par is null, i.e. cur is root. */
+					root = cur->right;
+					cur->right->par = NULL;
+				}
+				free(cur);
+				cur = NULL;
+				/* case 2 : if cur has only one child */
+			} else if(cur->left && !cur->right) {
+				if(cur->par) {
+					if(cur->par->left == cur) {
+						cur->par->left = cur->left;
+						cur->left->par = cur->par;
+					}
+					else {
+						cur->par->right = cur->right;
+						cur->right->par = cur->par;
+					}
+				} else {/* corner case when par is null, i.e. cur is root. */
+					root = cur->left;
+					cur->left->par = NULL;
+				}
+				free(cur);
+				cur = NULL;
+				/* case 3 : if cur has two children, replace it with successor */
+			} else {
+				successor = findsuccessor(cur);
+				if(successor) {
+					/* if successor is not right child of cur */
+					if(successor->par != cur) {
+						successor->par->left = successor->right;
+						if(successor->right) successor->right->par = successor->par;
+						successor->left = cur->left;
+						if(cur->left) cur->left->par = successor;
+						successor->right = cur->right;
+						if(cur->right) cur->right->par = successor;
+						/* if cur is not root */
+						if(cur->par) {
+							if(cur->par->right == cur) {
+								cur->par->right = successor;
+								successor->par = cur->par;
+							} else {
+								cur->par->left = successor;
+								successor->par = cur->par;
+							}
+							/* if cur is root */
+						} else {
+							root = successor;
+							successor->par = NULL;
+						}
+						free(cur);
+						cur = NULL;
+						/* recalc successor & right child's height. */
+						setht(successor->right);
+						setht(successor);
+					} else {
+						/* if successor is right child of cur */
+						successor->left = cur->left;
+						if(cur->left) cur->left->par = successor;
+						/* if cur is not root */
+						if(cur->par) {
+							if(cur->par->right == cur) {
+								cur->par->right = successor;
+								successor->par = cur->par;
+							} else {
+								cur->par->left = successor;
+								successor->par = cur->par;
+							}
+							/* if cur is root */
+						} else {
+							root = successor;
+							successor->par = NULL;
+						}
+
+						free(cur);
+						cur = NULL;
+						/* recalc only successor(right child) height. 
+						   right child is a successor. */
+						setht(successor);
+					}
+				}
+			}
+		} else if(tn.key < cur->key) {
+			xdelete(&cur->left, tn);
+			setht(cur);
+		} else {
+			xdelete(&cur->right, tn);
+			setht(cur);
+		}
+		if(cur) {
+			if(!cur->left && !cur->right) bal = 0;
+			else if(!cur->left && cur->right) bal = cur->right->ht - (-1); /* height of NULL is -1 */
+			else if(!cur->right && cur->left) bal = (-1) - cur->left->ht; /* height of NULL is -1 */
+			else bal = cur->right->ht - cur->left->ht;
+
+			if(bal == 2) {
+				temp = cur->right;
+				/* rotation left is enough for rebalance */
+				if(temp->right && temp->left) {
+					if(cur->right->right->ht < cur->right->left->ht) {
+						/* case 1 : zig-zag */
+						rrl(cur);
+					} else { /* case 2 : zig-zig */
+						rl(cur);
+					}
+				} else if(temp->right && !temp->left) {
+					/* case 2 : zig-zig */	
+					rl(cur);
+				} else {
+					rrl(cur);
+				}
+			} else if (bal == -2) {
+				temp = cur->left;
+				if(temp->right && temp->left) {
+					if(cur->left->left->ht < cur->left->right->ht) {
+						/* case 3 : zig-zag */
+						rlr(cur);
+					} else {
+						/* case 4 : zag-zag? */
+						rr(cur);
+					}
+				} else if(temp->left && !temp->right) {
+					rr(cur);
+				} else {
+					rlr(cur);
+				}
+			}
+		}
+	}
+}
+
+/* simple but not used for rebalancing */
+void delmerge(struct tnode n)
+{
+	struct tnode *cur = root;
+	struct tnode *par = NULL;
+	struct tnode *temp = NULL;
+
+	while(cur) {
+		if(cur->key < n.key){
+			par = cur;
+			cur = cur->right;
+		} else if(cur->key > n.key) {
+			par = cur;
+			cur = cur->left;
+		} else { /* found */
+			/* case 1 : if cur is leaf*/
+			if(!cur->left && !cur->right) {
+				if(!par) {/* there is only a root node */
+					free(cur);
+					root = NULL;
+				} else if(par->left == cur) {
+					par->left = NULL;
+					free(cur);
+				} else if(par->right == cur) {
+					par->right = NULL;
+					free(cur);
+				} 
+			/* case 2 : if cur has only one child */
+			} else if(!cur->left) {
+				if(par) {
+					if(par->left == cur) par->left = cur->right;
+					else par->right = cur->right;
+				} else {/* corner case when par is null, i.e. cur is root. */
+					root = cur->right;
+				}
+				free(cur);
+			} else if(!cur->right) {
+				if(par) {
+					if(par->left == cur) par->left = cur->left;
+					else par->right = cur->right;
+				} else {/* corner case when par is null, i.e. cur is root. */
+					root = cur->left;
+				}
+				free(cur);
+			/* case 3 : if cur has two children, merge subtrees. 
+			 */
+			} else {
+				temp = findpredecessor(cur);
+				temp->right= cur->right;
+				if(par) {
+					if(par->left == cur) par->left = cur->left;
+					else if(par->right == cur) par->right = cur->left;
+				} else { /* corner case when par is null, i.e. cur is root. */
+					if(temp != cur->left) temp->left = cur->left;
+					root = temp;
+				}
+				free(cur);
+			}
+			break;
+		}
+	}
+}
+
+struct tnode *search(struct tnode n)
+{
+	struct tnode *cur = root;
+
+	while(cur) {
+		if(cur->key < n.key) {
+			cur = cur->right;
+		} else if(cur->key > n.key) {
+			cur = cur->left;
+		} else {
+			break;
+		}
+	}
+
+	return cur;
+}
+
+void deletetree(struct tnode *pn)
+{
+	struct tnode *temp;
+	if(!pn) return;
+	else {
+		deletetree(pn->left);
+		temp = pn->right;
+		free(pn);
+		deletetree(temp);
+	}
+}
+
+void traverse(struct tnode *pn)
+{
+	if(!pn) return;
+	else {
+		traverse(pn->left);
+		printf("cur is %s, key : %d\n", pn->name, pn->key);
+		traverse(pn->right);
+	}
+}
+
+int main()
+{
+	int i, c, key;
+	char name[16];
+	struct tnode tn;
+	struct tnode *pn;
+	tn.name[0] = 'a'-1;
+
+#if 1
+	for(i=0 ; i<26 ; i++) {
+		tn.name[0] += 1; 
+		tn.name[1] = '\0';
+		tn.key = i+1;
+		//xinsertn(tn);
+		xinsert(&root, tn);
+	}
+#endif
+	c = getchar();
+	while(c != 'q') {
+		if(c == 'p') {
+			printf("input name :");
+			scanf("%s", tn.name);
+			printf("input key : ");
+			scanf("%d", &tn.key);
+			/*xinsertn(tn);*/
+			xinsert(&root, tn);
+		} else if(c == 'd') {
+			printf("remove key : ");
+			scanf("%d", &tn.key);
+			/*delmerge(tn);*/
+			xdelete(&root, tn);
+		} else if(c == 's') {
+			printf("search key : ");
+			scanf("%d", &tn.key);
+			pn = search(tn);
+			if(pn) printf("search name : %s\n",pn->name);
+			else printf("key %d is not in tree\n", tn.key);
+		}
+		c = getchar();
+	}
+	traverse(root);
+	deletetree(root);
+	return 0;
+}
