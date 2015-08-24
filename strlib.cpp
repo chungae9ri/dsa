a6734c5caa80ab02e7a88ee96f0868fd6456c576
Kwangdo Yi <chungae9ri@gmail.com>
2015-08-23 22:57:10 -0500

    add atoi, itoa to strlib.cpp

--- strlib.cpp
+++ strlib.cpp
@@ -51,9 +51,55 @@ char* strchr(char* p, int c)
 	return NULL;
 }
 
+int atoi(char *str)
+{
+	int i;
+	int ret=0;
+	for(i=0 ; i!='\0' ; i++) {
+		ret = ret * 10 + str[i]-'0';
+	}
+	return ret;
+}
+
+void itoa(int num, char *buf, int base)
+{
+	int rem, i=0, j, temp, minus = 0;
+
+	if(num == 0) {
+		buf[0] = '0';
+		buf[1] = '\0';
+		return;
+	}
+
+	if(num < 0) {
+		minus = 1;
+		num *= -1;
+	}
+
+	while(num > 0) {
+		rem = num % base;
+		buf[i++] = (rem>9) ? rem-10+'a' : rem + '0';
+		num = (int)(num/base);
+	}
+
+	for(j=0 ; j<i/2 ; j++) {
+		temp = buf[i-j-1];
+		buf[i-j-1] = buf[j];
+		buf[j] = temp;
+	}
+
+	buf[i] = '\0';
+	if(minus) {
+		for(j=i ; j>=0 ; j--) {
+			buf[j+1] = buf[j];
+		}
+		buf[0] = '-';
+	}
+}
+
 int main()
 {
-	char a[128], b[128];
+	char a[128], b[128], c[16];
 	char *cat, *chr;
 	int cnt;
 	scanf("%s", a);
@@ -68,5 +114,13 @@ int main()
 	if(chr != NULL)
 		cout << "strchr(a, 'c') :" << *chr << endl;
 	free(cat);
+
+	itoa(-12345, c, 10);
+	cout << "itoa(-12345) : " << c << endl;
+	itoa(12345, c, 2);
+	cout << "itoa(12345, base 2) : " << c << endl;
+	itoa(12345, c, 16);
+	cout << "itoa(12345, base 16) : " << c << endl;
+
 	return 0;
 }
