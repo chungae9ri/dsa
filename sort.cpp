b2dafa0560419407d69b0d24fc019eb24a7247fc
Kwangdo Yi <chungae9ri@gmail.com>
2015-08-28 09:37:45 -0500

    add heap sort to sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -5,35 +5,31 @@
 /* for using random number generator */
 #include <time.h>
 
-/* max random number */
-#define RNUM 200	
-
-
 using namespace std;
 #define MAXLEN	256 
 
 template <typename T> class Xsort{
 	T data[MAXLEN];
-	int len;
+	int last;
 	public :
 		Xsort(){
-			len = 0;
+			last = 0;
 			for(int i=0 ; i<MAXLEN; i++)
 				data[i] = 0;
 		}
 		~Xsort(){}
 		void Xinsert(T in) {
-			if(len > MAXLEN-1) return;
-			data[len++] = in;
+			if(last > MAXLEN-1) return;
+			data[last++] = in;
 		}
 		void Xdisplay() {
-			for(int i=0 ; i<len; i++) {
-				cout << data[i] << endl;
+			for(int i=0 ; i<last; i++) {
+				cout << i << "th : " << data[i] << endl;
 			}
 		}
 		void Xchksort() {
 			bool sorted = true;
-			for(int i=0 ; i<len-1 ; i++) {
+			for(int i=0 ; i<last-1 ; i++) {
 				if(data[i] > data[i+1]) {
 					sorted = false;
 					cout << "not sorted btwn " << i << " and " << i+1 << endl;
@@ -50,8 +46,8 @@ template <typename T> class Xsort{
 		void Xselectionsort() {
 			int i, j;
 			T temp;
-			for(i=0 ; i<len-1 ; i++) {
-				for(j=i+1 ; j<len ; j++) {
+			for(i=0 ; i<last-1 ; i++) {
+				for(j=i+1 ; j<last ; j++) {
 					if(data[j] < data[i]) {
 						Xswap(data[i], data[j]);
 					}
@@ -61,7 +57,7 @@ template <typename T> class Xsort{
 		void Xinsertionsort() {
 			int i, j;
 			T temp;
-			for(i=1 ; i<len ; i++) {
+			for(i=1 ; i<last ; i++) {
 				temp = data[i];
 				for(j=i ; j>0 && temp<data[j-1] ; j--) {
 					data[j] = data[j-1];
@@ -71,21 +67,21 @@ template <typename T> class Xsort{
 		}
 		void Xbubblesort() {
 			int i, j;
-			for(i=0 ; i<len ; i++) {
-				for(j=len-1 ; j>i ; j--) {
+			for(i=0 ; i<last ; i++) {
+				for(j=last-1 ; j>i ; j--) {
 					if(data[j] < data[j-1])
 						Xswap(data[j], data[j-1]);
 				}
 			}
 		}
 		void Xcombsort() {
-			int step=len, i, j, k;
+			int step=last, i, j, k;
 
 #if 1
 			/*while((step=(int)(step/1.3) > 1) || again == 1) {*/
 			while((step=(int)(step/1.3)) >= 1) {
 				/*if(step < 1) step = 1;*/
-				for(i=0, j=step ; j<len ; i++, j++) {
+				for(i=0, j=step ; j<last ; i++, j++) {
 					if(data[i] > data[j]) {
 						Xswap(data[i], data[j]);
 					}
@@ -95,7 +91,7 @@ template <typename T> class Xsort{
 			bool again;
 			/* 1st phase */
 			while((step=(int)(step/1.3)) > 1) {
-				for(j=len-1 ; j>=step ; j--) {
+				for(j=last-1 ; j>=step ; j--) {
 					k = j-step;
 					if(data[j] < data[k])
 						Xswap(data[j], data[k]);
@@ -103,8 +99,8 @@ template <typename T> class Xsort{
 			}
 			/* 2nd phase */
 			again = true;
-			for(i=0 ; i<len-1 && again ; i++){
-				for(j=len-1, again=false ; j>i ; --j){
+			for(i=0 ; i<last-1 && again ; i++){
+				for(j=last-1, again=false ; j>i ; --j){
 					if(data[j] < data[j-1]) {
 						Xswap(data[j], data[j-1]);
 						again = true;
@@ -120,11 +116,11 @@ template <typename T> class Xsort{
 			int h, i, j;
 			T t;
 			int step, seq[5]={5,4,3,2,1};
-			/*for(h=len ; h /= 2 ; ){*/
-			h = len;
+			/*for(h=last ; h /= 2 ; ){*/
+			h = last;
 			while((h=(int)(h/2)) >= 1) { /* similiar with comb sort */
 				/* insertion sort with gap h */
-				for(i=h ; i<len ; i++) {
+				for(i=h ; i<last ; i++) {
 					t = data[i];
 					for(j=i ; j>=h && t<data[j-h] ; j-=h) {
 						data[j] = data[j-h];
@@ -137,7 +133,7 @@ template <typename T> class Xsort{
 			int seq[5] = {5,4,3,2,1};
 			for (step = 0; seq[step] >= 1; step++) {
 				inc = seq[step];
-				for (i = inc; i < len ; i++) {
+				for (i = inc; i < last ; i++) {
 					j = i;
 					T val = data[i];
 					while ((j >= inc) && val < data[j - inc]) {
@@ -148,6 +144,66 @@ template <typename T> class Xsort{
 				}
 			}
 #endif
+		}
+
+		void Xheapsort() {
+			int i;
+			/* max heap */
+			Xheapify();
+			for(i=last-1 ; i>0 ; i--) {
+				Xswap(data[0], data[i]);
+				Xmovedown(0, i-1);
+			}
+		}
+
+		void Xmovedown(int idx, int stop) {
+			int i, lc, rc;
+			if(idx == stop) return;
+			lc = 2*idx+1;
+			rc = lc+1;
+			i = idx;
+			if(rc >= stop) rc = lc;
+			while(data[i] < data[lc] || data[i] < data[rc]) {
+				if(data[lc] >= data[rc] && data[lc] > data[i]) {
+					Xswap(data[lc], data[i]);
+					i = lc;
+					lc = 2*i + 1;
+					rc = lc + 1;
+					if(lc >= stop) break;
+				} else if(data[lc] <= data[rc] && data[rc] > data[i]) {
+					Xswap(data[rc], data[i]);
+					i = rc;
+					lc = 2*i + 1;
+					rc = lc + 1;
+					if(lc >= stop) break;
+				}
+			}
+		}
+
+		/* make a heap from an arbitrary array */
+		void Xheapify() {
+			int i, j, lc, rc;
+			int lastpar = (int)(last/2);
+			for(i=lastpar ; i>=0 ; i--) {
+				Xmovedown(i, last);
+			}
+			xchkheapify();
+		}
+
+		void xchkheapify() {
+			bool b = true;
+			int rc, lc;
+			for(int i=0 ; i<(int)(last/2) ; i++) {
+				lc = 2*i + 1;
+				rc = lc + 1;
+				if(rc >= last) rc = lc;
+				if(data[rc] > data[i] || data[lc] > data[i]) {
+					b = false;
+					cout << "not heap in " << i << endl;
+				}
+			}
+			if(b) cout << "correct max heap." << endl;
+			else cout << " not correct max heap." << endl;
 		}
 };
 
@@ -179,7 +235,7 @@ int main()
 #else
 	int in, i, stretch = 1024.f;
 	srand(time(NULL));
-	for(i=0 ; i<255 ; i++) {
+	for(i=0 ; i<256 ; i++) {
 		in = (int)((float)(rand())/(float)RAND_MAX*stretch);
 		data.Xinsert(in);
 	}
@@ -187,7 +243,7 @@ int main()
 #endif
 	cout << "q : quit, i : insert data" << endl;
 	cout << "1 : selectionsort, 2 : insertionsort, 3 : bubblesort" << endl;
-	cout << "4 : combsort, 5 : shellsort" << endl;
+	cout << "4 : combsort, 5 : shellsort, 6 : heapsort" << endl;
 	c = getchar();
 	while(c != 'q') {
 		if(c == 'i') {
@@ -203,6 +259,8 @@ int main()
 			data.Xcombsort();
 		} else if(c == '5') {
 			data.Xshellsort();
+		} else if(c == '6') {
+			data.Xheapsort();
 		}
 		c = getchar();
 	}
