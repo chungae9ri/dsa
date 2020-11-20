ad9d5dcbda0cb91265c5f53a7b14c8291ef06c56
kwangdo yi <chungae9ri@gmail.com>
2020-11-19 22:03:48 -0500

    changed sort algo by using template

--- asort.cpp
+++ asort.cpp
@@ -6,7 +6,70 @@
 
 using namespace std;
 
-uint32_t partition(vector<uint64_t> &v, uint32_t left, uint32_t right)
+#define NODE_NUM 	100000
+template <typename T>
+class Entry {
+	public:
+		T val;
+		Entry(T i): val{i} {};
+		Entry(): val{0}{};
+		bool operator<(const Entry& o) {
+			return (this->val < o.val);
+		}
+
+		bool operator>(const Entry& o) {
+			return (this->val > o.val);
+		}
+
+		bool operator<(const Entry& o) const {
+			return (this->val < o.val);
+		}
+
+		bool operator>(const Entry& o) const {
+			return (this->val > o.val);
+		}
+
+		bool operator==(const Entry& o) {
+			return (this->val == o.val);
+		}
+};
+
+
+void heapify(vector<Entry<uint64_t>> &v, int32_t len, uint32_t cur)
+{
+	uint32_t largest;
+	uint32_t lc = 2 * cur + 1;
+	uint32_t rc = 2 * cur + 2;
+
+	largest = cur;
+
+	if (lc < len && v[largest] < v[lc])
+		largest = lc;
+	if (rc < len && v[largest] < v[rc])
+		largest = rc;
+
+	if (largest != cur) {
+		swap(v[cur], v[largest]);
+		heapify(v, len, largest);
+	}
+}
+
+void heap_sort(vector<Entry<uint64_t>> &v)
+{
+	uint32_t len = (uint32_t)v.size();
+	/* build max heap */
+	for (int32_t i = len / 2 - 1; i >= 0; i--) 
+		heapify(v, len, i);
+
+	/* heap sort */
+	for (int32_t i = len - 1; i > 0 ; i--) {
+		swap(v[0], v[i]);
+		heapify(v, i, 0);
+	}
+
+}
+
+uint32_t partition(vector<Entry<uint64_t>> &v, uint32_t left, uint32_t right)
 {
 	uint32_t last = left;
 
@@ -18,7 +81,7 @@ uint32_t partition(vector<uint64_t> &v, uint32_t left, uint32_t right)
 	return last;
 }
 
-void qsort(vector<uint64_t> &v, uint32_t left, uint32_t right)
+void qsort(vector<Entry<uint64_t>> &v, uint32_t left, uint32_t right)
 {
 	uint32_t last; 
 	if (left >= right)
@@ -31,9 +94,9 @@ void qsort(vector<uint64_t> &v, uint32_t left, uint32_t right)
 	qsort(v, last + 1, right);
 }
 
-void merge(vector<uint64_t> &v, int32_t left, int32_t mid, int32_t right)
+void merge(vector<Entry<uint64_t>> &v, int32_t left, int32_t mid, int32_t right)
 {
-	vector<uint64_t> w(right - left + 1);
+	vector<Entry<uint64_t>> w(right - left + 1);
 
 	for (int i = 0, j = left, k = mid + 1; i < w.size(); i++) {
 		if (j == mid + 1) 
@@ -51,7 +114,7 @@ void merge(vector<uint64_t> &v, int32_t left, int32_t mid, int32_t right)
 	}
 }
 
-void merge_sort(vector<uint64_t> &v, int32_t left, int32_t right)
+void merge_sort(vector<Entry<uint64_t>> &v, int32_t left, int32_t right)
 {
 	if (left < right) {
 		uint32_t mid = left + (right - left) / 2;
@@ -66,9 +129,9 @@ void merge_sort(vector<uint64_t> &v, int32_t left, int32_t right)
  * if not using reference, it takes so long 
  * because every recursing copies the v to the subsequent calls 
  */
-uint64_t find_max(const vector<uint64_t> &v, int32_t i, int32_t j)
+Entry<uint64_t> find_max(const vector<Entry<uint64_t>>& v, int32_t i, int32_t j)
 {
-	int max; 
+	Entry<uint64_t> max(0); 
 
 	if (i + 1 == j) {
 		return (v[i] > v[j] ? v[i] : v[j]);
@@ -82,13 +145,13 @@ uint64_t find_max(const vector<uint64_t> &v, int32_t i, int32_t j)
 		return v[i];
 }
 
-uint64_t find_max2(const vector<uint64_t> v)
+Entry<uint64_t> find_max2(const vector<Entry<uint64_t>>& v)
 {
-	uint64_t max = 0;
+	Entry<uint64_t > max(0);
 
-	for (auto num: v) {
-		if (num > max)
-			max = num;
+	for (auto& e : v) {
+		if (e > max)
+			max = e;
 	}
 
 	return max;
@@ -96,10 +159,11 @@ uint64_t find_max2(const vector<uint64_t> v)
 
 int main()
 {
-	vector<uint64_t> v;
+	vector<Entry<uint64_t>> v;
 
-	for (int32_t i = 0; i < 10000; i++) {
-		v.push_back(rand() % 10000);
+	for (int32_t i = 0; i < NODE_NUM; i++) {
+		uint64_t val = rand() % NODE_NUM;
+		v.push_back(Entry<uint64_t>(val));
 	}
 
 	auto start = chrono::high_resolution_clock::now();
@@ -107,29 +171,59 @@ int main()
 	auto end = chrono::high_resolution_clock::now();
 	auto dur = chrono::duration_cast<chrono::microseconds>(end - start);
 
-	cout << "max: " << max1 << " takes: " << dur.count() << "usec" <<endl;
+	cout << "max: " << max1.val << " takes: " << dur.count() << "usec" <<endl;
 
 	auto start2 = chrono::high_resolution_clock::now();
 	auto max2 = find_max2(v);
 	auto end2 = chrono::high_resolution_clock::now();
 	auto dur2 = chrono::duration_cast<chrono::microseconds>(end2 - start2);
-	cout << "max: " << max2 << " takes: " << dur2.count() << "usec" <<endl;
+	cout << "max: " << max2.val << " takes: " << dur2.count() << "usec" <<endl;
 
 	/* use merge sort */
-	vector<uint64_t> v3 = v;
+	vector<Entry<uint64_t>> v3 = v;
 	auto start3 = chrono::high_resolution_clock::now();
 	merge_sort(v3, 0, v3.size() - 1);
 	auto max3 = v3.back();
 	auto end3 = chrono::high_resolution_clock::now();
 	auto dur3 = chrono::duration_cast<chrono::microseconds>(end3 - start3);
-	cout << "max: " << max3 << " takes: " << dur3.count() << "usec" <<endl;
+	cout << "max: " << max3.val << " takes: " << dur3.count() << "usec" <<endl;
+
+	for (int32_t i = 0; i < NODE_NUM - 1; i++) {
+		if (v3[i] > v3[i + 1]) {
+			cout << "wrong merge sort:v[" << i << "]: " << v3[i].val << "v[" << i + 1 << "]: " << v3[i + 1].val << "\n";
+			break;
+		}
+	}
 
 	/* use quick sort*/
-	vector<uint64_t> v4 = v;
+	vector<Entry<uint64_t>> v4 = v;
 	auto start4 = chrono::high_resolution_clock::now();
 	qsort(v4, 0, v4.size() - 1); 
 	auto max4 = v4.back();
 	auto end4 = chrono::high_resolution_clock::now();
 	auto dur4 = chrono::duration_cast<chrono::microseconds>(end4 - start4);
-	cout << "max: " << max4 << " takes: " << dur4.count() << "usec" <<endl;
+	cout << "max: " << max4.val << " takes: " << dur4.count() << "usec" <<endl;
+
+	for (int32_t i = 0; i < NODE_NUM - 1; i++) {
+		if (v4[i] > v4[i + 1]) {
+			cout << "wrong qsort:v[" << i << "]: " << v4[i].val << "v[" << i + 1 << "]: " << v4[i + 1].val << "\n";
+			break;
+		}
+	}
+
+	/* use heap sort */
+	vector<Entry<uint64_t>> v5 = v;
+	auto start5 = chrono::high_resolution_clock::now();
+	heap_sort(v5); 
+	auto max5 = v5.back();
+	auto end5 = chrono::high_resolution_clock::now();
+	auto dur5 = chrono::duration_cast<chrono::microseconds>(end5 - start5);
+	cout << "max: " << max5.val << " takes: " << dur5.count() << "usec" <<endl;
+
+	for (int32_t i = 0; i < NODE_NUM - 1; i++) {
+		if (v5[i] > v5[i + 1]) {
+			cout << "wrong heap sort:v[" << i << "]: " << v5[i].val << "v[" << i + 1 << "]: " << v5[i + 1].val << "\n";
+			break;
+		}
+	}
 }
